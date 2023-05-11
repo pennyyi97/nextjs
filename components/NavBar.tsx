3c05313216c3f796030553612882a931aaffa13b
vin <pennyyi97@gmail.com>
2023-05-11 16:48:05 +0900

    ENH: style 적용하는 방법 2

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,30 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from './NavBar.module.css'
+
 export default function NavBar(){
     const router = useRouter();
     return (
     <nav>
-        <Link href={"/"} className={`${styles.link} ${router.pathname === '/' ? styles.active : ''}`} >Home</Link> {/* 예전에는 a태그에 들어가야 속성적용이 가능했는데 지금은 그냥 Link에 바로 속성 적용 가능 */}
-        <Link href={"/about"} className={router.pathname === '/about' ? styles.active : ''}>About</Link>
+      {/* 예전에는 a태그에 들어가야 속성적용이 가능했는데 지금은 그냥 Link에 바로 속성 적용 가능 */}
+      {/* a tag 쓰려면 legacyBehavior가 들어가야만 사용할 수 있음 이건 아래 a태그에 style 적용하려고 추가한거임ㅠ */}
+      <Link href={"/"} legacyBehavior> 
+        <a className={router.pathname === "/" ? "active": ""}>Home</a>
+      </Link> 
+      <Link href={"/about"} legacyBehavior>
+        <a className={router.pathname === "/about" ? "active": ""}>About</a>  
+      </Link>
+      <style jsx>{`
+        nav{
+          background-color: tomato;
+        }
+        a{
+          text-decoration: none;
+        }
+        .active{
+          color: yellow;
+        }
+
+      `}</style>
     </nav>
     );
 }
