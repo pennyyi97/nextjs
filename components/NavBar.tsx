df290a2af4da2ee2802804d75ae6cfcc6bf7a007
vin <pennyyi97@gmail.com>
2023-05-11 15:32:22 +0900

    ENH: style 적용하는 방법 1

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-
+import styles from './NavBar.module.css'
 export default function NavBar(){
     const router = useRouter();
     return (
     <nav>
-        <Link href={"/"} style={{color: router.pathname === "/" ? "red" : "blue"}} className="hello">Home</Link> {/* 예전에는 a태그에 들어가야 속성적용이 가능했는데 지금은 그냥 Link에 바로 속성 적용 가능 */}
-        <Link href={"/about"} style={{color: router.pathname === "/about"? "red" : "blue"}}>About</Link>
+        <Link href={"/"} className={`${styles.link} ${router.pathname === '/' ? styles.active : ''}`} >Home</Link> {/* 예전에는 a태그에 들어가야 속성적용이 가능했는데 지금은 그냥 Link에 바로 속성 적용 가능 */}
+        <Link href={"/about"} className={router.pathname === '/about' ? styles.active : ''}>About</Link>
     </nav>
     );
 }
