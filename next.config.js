f5baffdf958d9081c59a98a366ba1903d719c942
vin <pennyyi97@gmail.com>
2023-05-12 11:29:24 +0900

    ENH: API 키 숨기기/ redirects, rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = "0ab38429f0e1a6919828c21e75f7408d";
+
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
